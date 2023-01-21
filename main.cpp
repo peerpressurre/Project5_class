e30a7dce4acba0a0416a7bf6d7f2a326c01c583c
student <student@PC-G-04>
2023-01-21 10:30:56 +0200

    1

---
+++ main.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char sym;
+	cout << "Enter symbol-> ";
+	cin >> sym;
+	int len, wid;
+	cout << "Enter length-> ";
+    cin >> len;
+	cout << "Enter widgth_> ";
+	cin >> wid;
+
+	for (size_t i = 1; i < len; i++)
+	{
+		for (size_t j = 1; j < wid; j++)
+		{
+			cout << sym;
+		}
+		cout << "\n";
+	} 
+	
+	return 0;
+}
