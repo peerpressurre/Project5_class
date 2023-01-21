53006afbdec9e40224ae078300562e5c91d2bddd
student <student@PC-G-04>
2023-01-21 11:38:40 +0200

    1f

--- main.cpp
+++ main.cpp
@@ -3,23 +3,6 @@
 using namespace std;
 int main()
 {
-    //const uint32_t size = 5;
-    //int arr[size];
-    //for (size_t i = 0; i < size; i++)
-    //{
-    //    cout << "item->";
-    //    cin >> arr[i];
-    //}
-    //system("cls");
-    //for (size_t i = 0; i < size; i++)
-    //{
-    //    cout << "[" << i << "] : " << arr[i] << " , " << (long long)&arr[i] << endl;
-    //}
-    //cout << "Size = " << sizeof(arr) << ", Type = " << typeid(arr).name() << endl;
-    //cout << endl;
-
-    //-----------------------------------------------------------------
-
     int sum = 0;
     const uint32_t size = 6;
     int arr[size];
