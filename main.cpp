ca413f89894dc78a5e43fd720ff72cf0b7027f60
student <student@PC-G-04>
2023-01-21 11:19:25 +0200

    2final

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
+
 #include <iostream>
 #include <time.h>
 using namespace std;
 int main()
-{
+    {
+        int temp, num, i, j;
+        const uint32_t size = 5;
+        int arr[size];
+       /* cout << "Enter each month income:" << endl;*/
+        for (size_t i = 0; i < size; i++)
+        {
+            cout << "->";
+            cin >> arr[i];
+        }
 
-}
+        for ( i = size - 1; i != -1; i--)
+        {
+            if (i != -858993460)
+            {
+                cout << arr[i] << "\t";
+            }
+        }
+        
+    }
